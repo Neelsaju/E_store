9613c2872cc24db26d0d94ddd206c38a07cb6c4d
Your Name <you@example.com>
2024-01-18 12:53:33 +0530

    initial commit

---
+++ ecom/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Customer,Product,Orders,Feedback,Contact
+# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Customer, CustomerAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Product, ProductAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Orders, OrderAdmin)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['id','name','email','subject','added_on','is_approved']
+
+admin.site.register(Feedback, FeedbackAdmin)
+# Register your models here.
